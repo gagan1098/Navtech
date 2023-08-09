c540f9b9b16d9861edfb16d9f697bbc8a725f5b0
gagan1098 <narulagagandeepsingh@gmail.com>
2023-08-09 15:12:30 +0530

    orderproduct class added

---
+++ Navtech/Data/DataContextOrderProductMapping.cs
@@ -0,0 +1,9 @@
+﻿namespace Navtech.Data
+{
+    public class DataContextOrderProductMapping:DbContext
+    {
+        public DataContextOrderProductMapping(DbContextOptions<DataContextOrderProductMapping> options) : base(options) { }
+
+        public DbSet<OrderProductMapping> orderproducts { get; set; }
+    }
+}
---
+++ Navtech/OrderProductMapping.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Navtech
+{
+    public class OrderProductMapping
+    {
+        public int id {  get; set; }
+
+        [ForeignKey("productId")]
+        public int productId { get; set; }
+        public virtual Product Product { get; set; }
+
+        [ForeignKey("orderId")]
+        public string orderId { get; set; }
+        public virtual Order Order  { get; set; }
+    }
+}
